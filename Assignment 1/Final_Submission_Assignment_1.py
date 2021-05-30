f7d5a6357a5f646c9c5ad46bfe44f11a5567eaa7
Nahid Hossain Taz <68403131+Nahid015@users.noreply.github.com>
2021-05-30 20:49:05 +0600

    Add files via upload

---
+++ Assignment 1/Final_Submission_Assignment_1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Nahid Hossain Taz- Assignment:1
+#Upskill IM - 2021
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Stylesheets defined in Matplotlib - Here I use 'ggplot'
+plt.style.use('ggplot')
+
+# Set up runtime comparison points for n=7
+n=7
+k = 4
+x = np.linspace(1, n, num= 1000)
+labels = [ 'Logarithmic', 'Linear', 'Quadratic', 'Cubic', 'Polynomial']
+big_o_notation = [ np.log2(x), x, x**2, x**3, x**k]
+
+# Plot setup
+plt.figure(figsize=(15, 10))
+plt.xlim(0, 7)
+plt.ylim(0, 15)
+
+for i in range(len(big_o_notation)):
+    plt.plot(x, big_o_notation[i], label=labels[i])
+
+plt.legend(loc=0)
+plt.ylabel('Relative Runtime')
+plt.xlabel('Input Size')
+plt.savefig('big-o-notation.png')
+
